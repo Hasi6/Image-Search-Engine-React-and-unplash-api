3fe42dc8515ffd26072aa6d77ec8fd5a63db1e39
Hasi6 <37216970+Hasi6@users.noreply.github.com>
2019-05-16 18:12:53 +0530

    update
    Now Users can see the categories in our page

--- ecommerce_node/routes/main.js
+++ ecommerce_node/routes/main.js
@@ -1,15 +1,21 @@
 var router = require('express').Router();
+var User = require('../models/user');
 
 
 
 
 router.get('/', function(req, res) {
-  res.render('main/home');
+    res.render('main/home');
 });
 
 router.get('/about', function(req, res) {
-  res.render('main/about');
+    res.render('main/about');
 });
 
+router.get('/users', function(req, res) {
+    User.find({}, function(err, users) {
+        res.json(users);
+    })
+})
 
 module.exports = router;
--- ecommerce_node/server.js
+++ ecommerce_node/server.js
@@ -13,6 +13,7 @@ var passport = require('passport');
 
 var secret = require('./config/secret');
 var User = require('./models/user');
+var Category = require('./models/category');
 
 var app = express();
 
@@ -42,6 +43,14 @@ app.use(passport.session());
 app.use(function(req, res, next) {
     res.locals.user = req.user;
     next();
+});
+
+app.use(function(req, res, next) {
+    Category.find({}, function(err, categories) {
+        if (err) return next(err);
+        res.locals.categories = categories;
+        next();
+    });
 });
 
 app.engine('ejs', engine);
