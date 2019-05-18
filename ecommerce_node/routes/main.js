3cc15906493d7a767fccc273f717abdd1d8f465d
Hasi6 <37216970+Hasi6@users.noreply.github.com>
2019-05-18 10:07:10 +0530

    create product.ejs file

--- ecommerce_node/routes/main.js
+++ ecommerce_node/routes/main.js
@@ -25,7 +25,14 @@ router.get('/products/:id', function(req, res, next) {
         });
 });
 
-
+router.get('/products/:id', function(req, res, next) {
+    Product.findById({ _id: req.params.id }, function(err, product) {
+        if (err) return next(err);
+        res.render('main/product', {
+            product: product
+        });
+    });
+});
 
 
 module.exports = router;
